39a2ed8420e86a39203cf0729c09933e4c029f9c
pjmarz <petermarino2@gmail.com>
2023-10-12 22:03:06 -0400

    idk

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 export TZ=US/Eastern
 export PUID=0
-export MEDIA=/mnt/LOAS/logs.msc
-export ROOT=/home/peter
 export PGID=0
+export ROOT=/home/peter
+export MEDIA=/mnt/LOAS/logs.msc
 export CHARON=/mnt/CHARON
