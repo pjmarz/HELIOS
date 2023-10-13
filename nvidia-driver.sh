40aa9204b12b133a6da8a41908f7d1d96b4588f2
pjmarz <petermarino2@gmail.com>
2023-10-12 22:02:27 -0400

    added execution to nvidia-driver

--- nvidia-driver.sh
+++ nvidia-driver.sh
@@ -4,3 +4,5 @@ sudo apt-get install -y wget
 wget https://us.download.nvidia.com/XFree86/Linux-x86_64/535.113.01/NVIDIA-Linux-x86_64-535.113.01.run
 
 chmod +x NVIDIA-Linux-x86_64-535.113.01.run
+
+sudo ./NVIDIA-Linux-x86_64-535.113.01.run
