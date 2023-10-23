69ffd8bf6fbf49181303cf0c6e82bdbeac7961b8
pjmarz <petermarino2@gmail.com>
2023-10-23 18:36:49 -0400

    idk

--- nvidia-driver.sh
+++ nvidia-driver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://www.nvidia.com/en-us/drivers/unix/
+
 sudo apt-get install -y wget
 wget https://us.download.nvidia.com/XFree86/Linux-x86_64/535.113.01/NVIDIA-Linux-x86_64-535.113.01.run
 
