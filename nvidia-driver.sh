7a86fac680d4bb0a58e7e6dc01a9ac70fc880fcb
pjmarz <petermarino2@gmail.com>
2023-11-12 15:35:00 -0500

    im dying

--- nvidia-driver.sh
+++ nvidia-driver.sh
@@ -2,9 +2,11 @@
 
 # https://www.nvidia.com/en-us/drivers/unix/
 
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+
 sudo apt-get install -y wget
-wget https://us.download.nvidia.com/XFree86/Linux-x86_64/535.113.01/NVIDIA-Linux-x86_64-535.113.01.run
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/535.129.03/NVIDIA-Linux-x86_64-535.129.03.run
 
-chmod +x NVIDIA-Linux-x86_64-535.113.01.run
+chmod +x NVIDIA-Linux-x86_64-535.129.03.run
 
-sudo ./NVIDIA-Linux-x86_64-535.113.01.run
+sudo ./NVIDIA-Linux-x86_64-535.129.03.run
