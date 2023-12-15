a8addb4008be65746df44f1aa5756dd8ed4ba6b3
pjmarz <petermarino2@gmail.com>
2023-12-15 11:45:58 -0500

    re-imagined

--- env.sh
+++ env.sh
@@ -2,6 +2,6 @@ export TZ=US/Eastern
 export PUID=0
 export PGID=0
 export ROOT=/home/peter
-export MEDIA=/mnt/LOAS/logs.msc
+export MEDIA=/mnt/LOAS
 export CHARON=/mnt/CHARON
 export HOST_ADDR=$(curl -s https://api.ipify.org)
