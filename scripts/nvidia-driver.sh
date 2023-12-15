d10f17a534fbbe32eea88c2637e25d4432743e21
pjmarz <petermarino2@gmail.com>
2023-12-14 22:24:05 -0500

    installed NVIDIA-Linux-x86_64-535.146.02.run

--- scripts/nvidia-driver.sh
+++ scripts/nvidia-driver.sh
@@ -2,10 +2,11 @@
 
 # https://www.nvidia.com/en-us/drivers/unix/
 # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+# sudo ./NVIDIA-Linux-XXXXXXX.run --uninstall
 
 sudo apt-get install -y wget
-wget -P ./ https://us.download.nvidia.com/XFree86/Linux-x86_64/535.129.03/NVIDIA-Linux-x86_64-535.129.03.run
+wget -P ./ https://us.download.nvidia.com/XFree86/Linux-x86_64/535.146.02/NVIDIA-Linux-x86_64-535.146.02.run
 
-chmod +x ./NVIDIA-Linux-x86_64-535.129.03.run
+chmod +x ./NVIDIA-Linux-x86_64-535.146.02.run
 
-sudo ./NVIDIA-Linux-x86_64-535.129.03.run
+sudo ./NVIDIA-Linux-x86_64-535.146.02.run
