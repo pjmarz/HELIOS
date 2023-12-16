e6425aae4da4b2fe28cec34861bcd33b0af61c83
pjmarz <petermarino2@gmail.com>
2023-12-16 13:27:36 -0500

    more script updates

--- scripts/master-compose-up.sh
+++ scripts/master-compose-up.sh
@@ -10,11 +10,16 @@ media_management_center_path="/home/peter/Documents/dev/HELIOS/Media Management 
 # Define the log file
 LOGFILE="/home/peter/Documents/dev/HELIOS/script_logs/master-compose-up.log"
 
+# Function to prepend the current date and time to log messages
+log_with_date() {
+    echo "$(date "+%Y-%m-%d %H:%M:%S") - $1"
+}
+
 # Function to run docker-compose up in a directory and log the output
 run_docker_compose_up() {
-    echo "Starting docker-compose up in $1..." | tee -a "$LOGFILE"
-    docker compose -f "$1/docker-compose.yml" up -d | tee -a "$LOGFILE"
-    echo "docker-compose up finished in $1" | tee -a "$LOGFILE"
+    log_with_date "Starting docker-compose up in $1..." | tee -a "$LOGFILE"
+    docker compose -f "$1/docker-compose.yml" up -d 2>&1 | tee -a "$LOGFILE"
+    log_with_date "docker-compose up finished in $1" | tee -a "$LOGFILE"
 }
 
 # Run docker-compose up -d in both directories and log the output
@@ -23,4 +28,4 @@ run_docker_compose_up "$media_management_center_path" &
 
 # Wait for all background processes to finish
 wait
-echo "All docker-compose services are up and running." | tee -a "$LOGFILE"
+log_with_date "All docker-compose services are up and running." | tee -a "$LOGFILE"
