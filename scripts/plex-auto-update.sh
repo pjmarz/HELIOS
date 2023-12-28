af11d619615f4aceb38daf808035ea9e004e3390
pjmarz <petermarino2@gmail.com>
2023-12-28 16:32:04 -0500

    stuff

--- scripts/plex-auto-update.sh
+++ scripts/plex-auto-update.sh
@@ -3,28 +3,34 @@
 # Define the logfile and Plex server details
 LOGFILE="/home/peter/Documents/dev/HELIOS/script_logs/plex-auto-update.log"
 PLEX_URL="https://192.168.1.45:32400"
-TOKEN="kn5myHmFyQy2HgqzWZ4T"
-LIBRARY_SECTION_ID="1"
+TOKEN="reKFC-C828Gqv6aJ2ehG"
+LIBRARY_SECTION_IDS=("1" "2" "3")  # Array of library IDs
 
 # Redirect all output to the logfile
 exec > "$LOGFILE" 2>&1
 
 echo "$(date) - Starting Plex library update..."
 
-# Scan the library section for new content
-echo "Starting Plex library scan..."
-curl -k -L -X GET "${PLEX_URL}/library/sections/${LIBRARY_SECTION_ID}/refresh" -H "X-Plex-Token: ${TOKEN}"
+# Function to update a single library section
+update_library_section() {
+    local section_id=$1
+    echo "Starting Plex library scan for section ${section_id}..."
+    curl -k -L -X GET "${PLEX_URL}/library/sections/${section_id}/refresh" -H "X-Plex-Token: ${TOKEN}"
 
-sleep 60
+    sleep 60
 
-# Analyze media files in the library section
-echo "Starting Plex media analysis..."
-curl -k -L -X PUT "${PLEX_URL}/library/sections/${LIBRARY_SECTION_ID}/analyze" -H "X-Plex-Token: ${TOKEN}"
+    echo "Starting Plex media analysis for section ${section_id}..."
+    curl -k -L -X PUT "${PLEX_URL}/library/sections/${section_id}/analyze" -H "X-Plex-Token: ${TOKEN}"
 
-sleep 2700
+    sleep 3600
 
-# Refresh all metadata for the library section
-echo "Starting Plex metadata refresh..."
-curl -k -X GET "${PLEX_URL}/library/sections/${LIBRARY_SECTION_ID}/refresh?force=1&X-Plex-Token=${TOKEN}"
+    echo "Starting Plex metadata refresh for section ${section_id}..."
+    curl -k -X GET "${PLEX_URL}/library/sections/${section_id}/refresh?force=1&X-Plex-Token=${TOKEN}"
+}
 
-echo "Plex library update tasks completed!"
+# Iterate over each library section and update
+for section_id in "${LIBRARY_SECTION_IDS[@]}"; do
+    update_library_section "$section_id"
+done
+
+echo "Plex library update tasks for all sections completed!"
