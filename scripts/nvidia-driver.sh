dc1beb47ec0089a754d75149cc0f426f0354048a
pjmarz <petermarino2@gmail.com>
2024-01-07 01:35:17 -0500

    claimed plex token

--- scripts/nvidia-driver.sh
+++ scripts/nvidia-driver.sh
@@ -5,6 +5,10 @@
 # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
 # sudo ./NVIDIA-Linux-XXXXXXX.run --uninstall
 
+# https://old.reddit.com/r/PleX/comments/12ikwup/plex_docker_hardware_transcoding_issue/jg2l7lc/
+
+# docker run --rm --gpus all nvidia/cuda:12.2.2-base-ubuntu22.04 nvidia-smi
+
 sudo apt-get install -y wget
 wget -P ./ https://us.download.nvidia.com/XFree86/Linux-x86_64/535.146.02/NVIDIA-Linux-x86_64-535.146.02.run
 
