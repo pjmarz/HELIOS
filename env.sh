c4078102e5a282c67399af1e8a1c963c2e9aba8a
pjmarz <petermarino2@gmail.com>
2024-12-30 22:27:21 -0500

    what is happening anymore

--- env.sh
+++ env.sh
@@ -1,5 +1,10 @@
-export TZ=America/New_York
-export ROOT=/home/peter
-export LOAS=/mnt/LOAS
+# Timezone
+export TZ="America/New_York"
+
+# Base Directories
+export ROOT="/home/peter"
+export LOAS="/mnt/LOAS"
+
+# Plex Configuration
 export TRANSCODE="/home/peter/config/plexdb/Library/Application Support/Plex Media Server/Transcode"
-export PLEX_TOKEN=fmdSEyKQuRrNnQiWLys8
+export PLEX_TOKEN="fmdSEyKQuRrNnQiWLys8"
